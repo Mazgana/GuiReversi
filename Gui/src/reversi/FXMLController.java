becd3885f5b85c665a5c2db4ec7fbf0bd8dd9cdf
mazgana <adi.mazgani@gmail.com>
2018-01-16 22:17:55 +0200

    reading settings from file & adding other chip colors

--- Gui/src/reversi/FXMLController.java
+++ Gui/src/reversi/FXMLController.java
@@ -1,5 +1,8 @@
 package reversi;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,18 +19,66 @@ public class FXMLController implements Initializable{
 	@FXML
 	private GameFlow game;
 	
+	private String opening;
+	private int size;
+	private String firstColor;
+	private String secondColor;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.game = new GameFlow();
+		//getting game's settings from the settings file
+		setGameSettings();
+		
+		this.game = new GameFlow(this.opening, this.size, this.firstColor, this.secondColor);
 		this.game.setPrefWidth(500);
 		this.game.setPrefHeight(500);
 		root.getChildren().add(this.game);
 		
 		try {
-			this.game.run();
+			this.game.run(this.firstColor, this.secondColor);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	public void setGameSettings() {
+		FileReader settingsFile = null;
+		BufferedReader br = null;
+		
+		try {
+			settingsFile = new FileReader("settings.txt");
+			br = new BufferedReader(settingsFile);
+			
+			//Opening player
+			String temp = br.readLine();
+			this.opening = temp.substring(16, temp.length());
+			
+			//board size
+			temp = br.readLine();
+			this.size = Integer.valueOf(temp.substring(12, temp.length()));
+			
+			//first player color
+			temp = br.readLine();
+			this.firstColor = temp.substring(13, temp.length());
+			
+			//second player color
+			temp = br.readLine();
+			this.secondColor = temp.substring(14, temp.length());
+			
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (settingsFile != null)
+					settingsFile.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
 	}
 }
--- Gui/src/reversi/Player.java
+++ Gui/src/reversi/Player.java
@@ -3,19 +3,26 @@ package reversi;
 public class Player {
 
 	private Status type;
+	private String color;
 	
 	public Player() {
 		this.type = Status.BLACK;
+		this.color = "black";
 	}
 	
-	public Player(Status s) {
+	public Player(Status s, String c) {
 		this.type = s;
+		this.color = c;
 	}
 	
 	public Status getChip() {
 	    return this.type;
 	}
 
+	public String getColor() {
+		return this.color;
+	}
+	
 	public Status getOppositeType() {
 		if (this.type == Status.BLACK) {
 			return Status.WHITE;
